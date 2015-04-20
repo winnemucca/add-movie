925dc81c1a29c34b667ce1778b391f49d17555a3
Steven Kauyedauty <skauyedauty@gmail.com>
2015-04-20 12:58:40 -0600

    first

---
+++ app.js
@@ -0,0 +1,14 @@
+angular.module('app',[]);
+
+angular.module('app').controller("MainController",function(){
+	var vm = this;
+	vm.title = "angular js tutorial example";
+	vm.searchInput = "";
+})
+	.controller('MainController',function(){
+		var vm = this;
+		vm.title = "AngularJs Tutorial Example";
+	});
+
+
+
